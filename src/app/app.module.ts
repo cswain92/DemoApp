536e38bf16e737401b337192d987066b09c4ea23
chiranjib-navsoft <chiranjib.swain@navsoft.in>
2022-06-07 20:20:29 +0530

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrhdet6ZsrQ7TWzT1RNB5UiIXJgbZ53Ek",
+  authDomain: "fir-264ec.firebaseapp.com",
+  projectId: "fir-264ec",
+  storageBucket: "fir-264ec.appspot.com",
+  messagingSenderId: "961278004125",
+  appId: "1:961278004125:web:da7b219084858fa700bd74"
+};
 
 @NgModule({
   declarations: [
@@ -22,7 +33,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
